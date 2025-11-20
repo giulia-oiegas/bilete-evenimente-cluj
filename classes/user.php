fdf1814d588ae2464dca674f7d26fa3bd7a81328
ooirina <200990762+ooirina@users.noreply.github.com>
2025-11-20 19:23:54 +0200

    Autentificare

--- classes/user.php
+++ classes/user.php
@@ -1,1 +1,31 @@
+<?php
+/**
+ Are rolul de a detine si gestiona datele unui singur utilizator.
+*/
+class User {
+// Proprietatile corespund coloanelor din tabela USERS
+public int $id;
+public string $username;
+public string $email;
+public string $password; // Parola hash-uita
+public string $role;
 
+/**
+Constructor pentru a initializa un obiect User.
+*/
+public function __construct(array $data) {
+$this->id = $data['id'] ?? 0;
+$this->username = $data['username'] ?? '';
+$this->email = $data['email'] ?? '';
+$this->password = $data['password'] ?? '';
+$this->role = $data['role'] ?? 'user';
+}
+
+    /**
+      Metoda ajutatoare pentru a verifica daca utilizatorul este admin.
+     */
+    public function isAdmin(): bool {
+        return $this->role === 'admin';
+    }
+}
+?>
