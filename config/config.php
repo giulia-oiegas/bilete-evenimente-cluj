f77f0795d3a958cc46d2f17078371ac63c209402
nessa-pvd <moisivanesa@gmail.com>
2025-11-20 18:19:13 +0200

    index.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,15 @@
 
+<?php
+// /config/db_config.php
+// DB_NAME este setat la 'ticket_shop'
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ticket_shop');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Configurare Stripe
+define('STRIPE_SECRET_KEY', 'sk_test_VOTRA_CHEIE_SECRETA');
+
+// Configurare Email (Lab 2)
+define('CONTACT_EMAIL', 'contact@biletecluj.ro');
+?>
