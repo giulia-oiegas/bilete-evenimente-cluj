32ca3e6bacb57e658f7258eb0b50319fc47e5b4c
nessa-pvd <moisivanesa@gmail.com>
2025-11-24 15:22:04 +0200

    stergere calendar

--- config/stripe_config_template.php
+++ config/stripe_config_template.php
@@ -1,9 +1,5 @@
 <?php
-define("STRIPE_SECRET_KEY", "INSERT_YOUR_OWN_STRIPE_SECRET_KEY_HERE");
-{
-    die(" Stripe nu este configurat. Va rugăm să puneți cheile voastre de test în stripe_config_template.php");
-}
-define("STRIPE_PUBLISHABLE_KEY", "INSERT_YOUR_OWN_PUBLISHABLE_KEY_HERE");
-define("STRIPE_CURRENCY", "eur");
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51SVZtN2LNmOzr1lfURd3XZNg8U5yLkGepWpDXgOJnSCtpudrgXrWYjih64xNrnCKAoYN5dRBLHyjKtpabfswlKb7006ZaZZ8jE');
+define('STRIPE_SECRET_KEY', 'sk_test_51SVZtN2LNmOzr1lfHlyR70mlkM5MqthEtL6BzaTdxMMLOEfuxWE40LRjEX4BclbIxNwsHlgNwFX1nwJlT7twVs1400mGK9Hy4k');
+define('STRIPE_CURRENCY', 'ron');
 ?>
-
--- public/header.php
+++ public/header.php
@@ -32,7 +32,6 @@ $userRole = $_SESSION['user_role'] ?? 'guest';
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link active" href="index.php">Evenimente</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Calendar</a></li>
                     <li class="nav-item"><a class="nav-link" href="about.php">Despre noi</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                 </ul>
