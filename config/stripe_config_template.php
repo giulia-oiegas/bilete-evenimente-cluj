6d5578a2603e3a5a15bb3f1c34c60d7a701fb94d
Giulia-Nicola Oiegaș <giulia.oiegas@outlook.com>
2025-11-25 13:21:49 +0200

    comentare date sensibile stripe

--- config/stripe_config_template.php
+++ config/stripe_config_template.php
@@ -1,5 +1,6 @@
 <?php
+/*
 define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51SVZtN2LNmOzr1lfURd3XZNg8U5yLkGepWpDXgOJnSCtpudrgXrWYjih64xNrnCKAoYN5dRBLHyjKtpabfswlKb7006ZaZZ8jE');
 define('STRIPE_SECRET_KEY', 'sk_test_51SVZtN2LNmOzr1lfHlyR70mlkM5MqthEtL6BzaTdxMMLOEfuxWE40LRjEX4BclbIxNwsHlgNwFX1nwJlT7twVs1400mGK9Hy4k');
-define('STRIPE_CURRENCY', 'ron');
+define('STRIPE_CURRENCY', 'ron'); */
 ?>
