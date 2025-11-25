856a3ec5e64c2d1987e9d5faa82ac4ed98e96eb3
Giulia-Nicola Oiegaș <giulia.oiegas@outlook.com>
2025-11-25 15:00:01 +0200

    adaugare stil css si chei secrete stripe

--- config/stripe_config_template.php
+++ config/stripe_config_template.php
@@ -1,12 +1,5 @@
 <?php
-
-define('STRIPE_PUBLISHABLE_KEY', 'INLOCUITI_CU_CHEIA_PUBLICA_STRIPE_AICI');
-define('STRIPE_SECRET_KEY', 'INLOCUITI_CU_CHEIA_SECRETA_STRIPE_AICI');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51SVZtN2LNmOzr1lfURd3XZNg8U5yLkGepWpDXgOJnSCtpudrgXrWYjih64xNrnCKAoYN5dRBLHyjKtpabfswlKb7006ZaZZ8jE');
+define('STRIPE_SECRET_KEY', 'sk_test_51SVZtN2LNmOzr1lfHlyR70mlkM5MqthEtL6BzaTdxMMLOEfuxWE40LRjEX4BclbIxNwsHlgNwFX1nwJlT7twVs1400mGK9Hy4k');
 define('STRIPE_CURRENCY', 'ron');
-
-
-if (STRIPE_PUBLISHABLE_KEY === 'INLOCUITI_CU_CHEIA_PUBLICA_STRIPE_AICI') {
-    die('EROARE: Configurare Stripe incompleta. Va rugam inlocuiti placeholder-urile din fisierul de configurare.');
-}
-
 ?>
